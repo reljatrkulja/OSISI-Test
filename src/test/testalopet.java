129f87fdddc925a246979d956b181e806aa3b36b
reljatrkulja <reljatrkulja1@gmail.com>
2022-04-12 16:17:51 +0200

    Update testalopet.java

--- src/test/testalopet.java
+++ src/test/testalopet.java
@@ -1,7 +1,18 @@
 package test;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Vector;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class testalopet {
 	public static void main(String[] args) {
-		System.out.println('cao');
+		System.out.println("cao");
+		
 	}
 }
